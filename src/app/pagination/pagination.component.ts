32de37850aa4cfbde5eda5db3dd50542daaa1859
Starukhin Denis <90477034+Flockandglock@users.noreply.github.com>
2024-01-20 11:53:45 +0400

    добавил оставшиеся компоненты

---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [],
+  template: `
+    <div>app-pagination is working</div>
+  `,
+  styleUrl: './pagination.component.scss'
+})
+export class PaginationComponent {
+
+}
